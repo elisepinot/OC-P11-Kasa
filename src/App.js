71a07b12b1005a00396d36bf155a495add405a6b
elisepinot <elise.pinot@live.fr>
2023-11-14 15:50:56 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+function App() {
+   return (
+      <div className='App'>
+         <header className='App-header'>
+            <h1>Hello World</h1>
+            <p>Comment ça va ? Et là imaginons j\&aposajoute quelque chose ?</p>
+            <h2>ça fonctionne bien alors ?</h2>
+         </header>
+      </div>
+   );
+}
+
+export default App;
